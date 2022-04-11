e7136e4626e7634e11bf8ade4c1c160d1cf4fc6e
le <980292214@qq.com>
2022-04-11 16:47:01 +0800

    第一次提交

---
+++ service/service_hosp/src/main/java/com/le/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,15 @@
+package com.le.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 乐
+ * @version 1.0
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class,args);
+    }
+}
